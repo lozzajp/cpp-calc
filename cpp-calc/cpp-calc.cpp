76dbe8ce463eba32dc2db23f10cc784cfe9c184f
Laurence <ljpw2020@hotmail.com>
2019-06-19 21:29:01 +0100

    Basic addition

--- cpp-calc/cpp-calc.cpp
+++ cpp-calc/cpp-calc.cpp
@@ -1,21 +1,27 @@
-// cpp-calc.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    std::cout << "Hello Calculator\n\n" << "Please select an operation\n\n"; 
+
+	std::cout << "1. Addition\n" << "2. Subtraction\n" << "3. Multiplication\n" << "4. Division\n\n";
+
+	std::cout << "Operation: ";
+	std::string selection;
+	std::getline(std::cin, selection);
+	std::cout << "\n";
+
+	std::cout << "Please enter the LHS number: ";
+	float lhsNumber;
+	std::cin >> lhsNumber;
+	std::cout << "\n";
+
+	std::cout << "Please enter the RHS number: ";
+	float rhsNumber;
+	std::cin >> rhsNumber;
+	std::cout << "\n";
+
+	std::cout << "Addition Result: " << lhsNumber + rhsNumber;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
